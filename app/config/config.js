f6dc5a8db8599e662ccd480ef0db14cc89a690fa
nnikitha <47518124+nnikitha@users.noreply.github.com>
2020-06-18 06:47:04 +0530

    Initial Commit

---
+++ app/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "ec2-18-214-211-47.compute-1.amazonaws.com",
+    USER: "jeimizpbuxlhcl",
+    PASSWORD: "9466f74fd920d827fb88b4cbb7482a91d64960ba10c6402e745a32aa59a0e29d",
+    DB: "dbgoc8v8laa324",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
